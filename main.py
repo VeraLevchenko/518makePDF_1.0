081385bc500a56fc840b0e367eadb5528c16b0cf
11 <180982@mail.ru>
2024-06-06 19:27:23 +0300

    1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     pdf.add_font("Sans", style="I", fname="C:/Fonts/NotoSans-Italic.ttf", uni=True)
     pdf.add_font("Sans", style="BI", fname="C:/Fonts/NotoSans-BoldItalic.ttf", uni=True)
 
-    df = pd.read_excel('test.xlsx', index_col=0, sheet_name='Sheet1')
+    df = pd.read_excel('test.xlsx', index_col=0, sheet_name='Лист7')
     pd.options.display.expand_frame_repr = False
     pd.set_option('display.max_columns', 6)
     pd.set_option('display.max_colwidth', 30)
@@ -24,70 +24,66 @@ if __name__ == '__main__':
 
     for i in range(0, len(df.index)):
         print(f"{i}из{len(df.index)}")
-        if str(df.iloc[i]['Сыскин1']) == 'нет' \
-                and str(df.iloc[i]['Сыскин2']) == 'нет' \
-                and str(df.iloc[i]['Управляющая компания']) != 'nan':
-            adres = str(df.iloc[i]['Структурированный адрес'])
-            upr_komp = str(df.iloc[i]['Управляющая компания'])
+        adres = str(df.iloc[i]['Структурированный адрес'])
                 # добавляем пустую страницу
-            pdf.add_page()
+        pdf.add_page()
                 # задаем шрифт `Sans` ,
                 # `Bold` (жирный) и размером 16
 
-            pdf.image('Герб.jpg', x=100, y=None, w=10,  type='')
-            pdf.set_font("Sans", "", 11)
-            pdf.cell(190, 7, "КЕМЕРОВСКАЯ ОБЛАСТЬ-КУЗБАСС", align='C')
-            pdf.ln(5)
-            pdf.cell(190, 7, "НОВОКУЗНЕЦКИЙ ГОРОДСКОЙ ОКРУГ", align='C')
-            pdf.ln(5)
-            pdf.cell(190, 7, "АДМИНИСТРАЦИЯ ГОРОДА НОВОКУЗНЕЦКА", align='C')
-            pdf.ln()
-            pdf.cell(190, 7, '', border='T', ln=0, align='C')
-            pdf.ln(1)
-            pdf.cell(190, 7, '', border='T', ln=0, align='C')
-            pdf.ln()
+        pdf.image('Герб.jpg', x=100, y=None, w=10,  type='')
+        pdf.set_font("Sans", "", 11)
+        pdf.cell(190, 7, "КЕМЕРОВСКАЯ ОБЛАСТЬ-КУЗБАСС", align='C')
+        pdf.ln(5)
+        pdf.cell(190, 7, "НОВОКУЗНЕЦКИЙ ГОРОДСКОЙ ОКРУГ", align='C')
+        pdf.ln(5)
+        pdf.cell(190, 7, "АДМИНИСТРАЦИЯ ГОРОДА НОВОКУЗНЕЦКА", align='C')
+        pdf.ln()
+        pdf.cell(190, 7, '', border='T', ln=0, align='C')
+        pdf.ln(1)
+        pdf.cell(190, 7, '', border='T', ln=0, align='C')
+        pdf.ln()
 
-            pdf.set_font("Sans", "B", 13)
-            pdf.cell(190, 7, "Уважаемый Собственник!", ln=20, align='C' )
-            pdf.ln()
+        pdf.set_font("Sans", "B", 13)
+        pdf.cell(190, 7, "Уважаемый Собственник!", ln=20, align='C' )
+        pdf.ln()
 
-            pdf.set_font("Sans", "", 11)
-            pdf.cell(190, 7, "Право собственности на принадлежащую Вам квартиру по адресу:")
-            pdf.ln(12)
+        pdf.set_font("Sans", "", 11)
+        pdf.cell(190, 7, "Право собственности на принадлежащую Вам квартиру по адресу:")
+        pdf.ln(12)
 
-            pdf.set_font("Sans", "I", 11)
-            pdf.cell(190, 7, adres, ln=20, align='C')
-            pdf.ln()
+        pdf.set_font("Sans", "I", 11)
+        pdf.cell(190, 7, adres, ln=20, align='C')
+        pdf.ln()
 
-            pdf.set_font("Sans", "", 11)
-            pdf.cell(190, 7, "не зарегистрировано в Едином государственном реестре недвижимости (далее - ЕГРН).")
-            pdf.ln(10)
+        pdf.set_font("Sans", "", 11)
+        pdf.cell(190, 7, "не зарегистрировано в Едином государственном реестре недвижимости (далее - ЕГРН).")
+        pdf.ln(10)
 
-            pdf.set_font("Sans", "", 11)
-            pdf.multi_cell(190, 7, txt1)
-            pdf.ln(10)
+        pdf.set_font("Sans", "", 11)
+        pdf.multi_cell(190, 7, txt1)
+        pdf.ln(10)
 
-            pdf.set_font("Sans", "B", 12)
-            pdf.multi_cell(190, 7, "С целью защиты Ваших прав и законных интересов Вам необходимо "
-                             "зарегистрировать право собственности на принадлежащую Вам квартиру в ЕГРН!", align='C')
-            pdf.ln(10)
+        pdf.set_font("Sans", "B", 12)
+        pdf.multi_cell(190, 7, "С целью защиты Ваших прав и законных интересов Вам необходимо "
+                         "зарегистрировать право собственности на принадлежащую Вам квартиру в ЕГРН!", align='C')
+        pdf.ln(10)
 
-            pdf.set_font("Sans", "", 11)
-            pdf.multi_cell(190, 7, txt2)
-            pdf.ln(2)
+        pdf.set_font("Sans", "", 11)
+        pdf.multi_cell(190, 7, txt2)
+        pdf.ln(2)
 
-            pdf.set_font("Sans", "", 11)
-            pdf.cell(190, 7, "По всем вопросам Вы можете обращаться по телефону:")
-            pdf.ln()
-            pdf.cell(190, 7, "8 (3843) 45-86-73, Новокузнецкий отдел Управления Росреестра по Кемеровской области")
-            pdf.ln(20)
+        pdf.set_font("Sans", "", 11)
+        pdf.cell(190, 7, "По всем вопросам Вы можете обращаться по телефону:")
+        pdf.ln()
+        pdf.cell(190, 7, "8 (3843) 45-86-73, Новокузнецкий отдел Управления Росреестра по Кемеровской области")
+        pdf.ln(20)
 
-            pdf.cell(190, 7, "Администрация города Новокузнецка", align='R')
-            pdf.ln()
+        pdf.cell(190, 7, "Администрация города Новокузнецка", align='R')
+        pdf.ln()
 
-            pdf.set_font("Sans", "I", 11)
-            pdf.cell(190, 7, upr_komp, align='R')
-            pdf.ln()
+        # pdf.set_font("Sans", "I", 11)
+        # pdf.cell(190, 7, upr_komp, align='R')
+        # pdf.ln()
 
-    pdf.output("Уведомления\УК Гестия.pdf")
-    os.startfile("Уведомления\УК Гестия.pdf")
+    pdf.output("Новые уведомления\Новоильинский_518_жилые помещения.pdf")
+    os.startfile("Новые уведомления\Новоильинский_518_жилые помещения.pdf")
